4f41ea5a48fc81ef9c7e23801ea47b8d5239184a
Gautam Bose <gautam.bos3@gmail.com>
2019-04-09 21:50:33 -0400

    andrew final before consuemrs

--- App.js
+++ App.js
@@ -7,9 +7,13 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import { VisionCamera } from "react-native-vision";
-import { whileStatement } from '@babel/types';
+import { whileStatement, objectExpression } from '@babel/types';
+
+
+
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,25 +24,57 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  onButtonPress = () => {
+    
+  
+  };
+
   render() {
+    // console.log("andrew");
+
+
     return (
-      <VisionCamera style={{ flex: 1 }} classifier="MobileNet" isCameraFront={false}>
-        {({ label, confidence }) => (
-          <Text
-            style={{
-              width: "75%",
-              fontSize: 50,
-              position: "absolute",
-              right: 50,
-              bottom: 100, 
-              color: "white", 
-            }}
-          >
-            {label + " :" + (confidence * 100).toFixed(0) + "%"}
-          </Text>
-        )}
-      </VisionCamera>
-     
+      // <Image 
+      // source={require('/private/var/mobile/Containers/Data/Application/71BC002D-AC77-4828-A880-2D037D7B4755/tmp/ReactNative/F927B8C3-6864-470E-B4C8-ADBC0CC95663.jpg')}
+      // />
+        <VisionCamera style={{ flex: 1 }} classifier="MobileNet" isCameraFront={false}>
+
+          {(object) => {
+            {/* console.log(object);  */ }
+            return (
+              /*            <Text
+                            style={{
+                              width: "75%",
+                              fontSize: 50,
+                              position: "absolute",
+                              right: 50,
+                              bottom: 100,
+                              color: "white",
+                            }}
+                          >
+                            {object.label + " :" + (object.confidence * 100).toFixed(0) + "%"}
+                          </Text> */
+
+              <TouchableOpacity
+              style={{
+                              width: "75%",
+                              fontSize: 50,
+                              position: "absolute",
+                              right: 50,
+                              bottom: 100,
+                              color: "white",
+                            }}
+                onPress={this.onButtonPress}>
+                <Text> Take Photo </Text>
+              </TouchableOpacity>
+
+
+            )
+          }}
+        </VisionCamera>
+      
+
     );
   }
 }
